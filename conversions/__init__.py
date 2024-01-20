5f0e5da95d83a6022c21231e492b42c73f4e16a1
UzziKyle <uzzielkyleynciong0824@gmail.com>
2024-01-20 12:03:35 +0800

    fix imports in __init__

--- conversions/__init__.py
+++ conversions/__init__.py
@@ -1,7 +1,7 @@
-from from_binary import FromBinary
-from from_octal import FromOctal
-from from_decimal import FromDecimal
-from from_hex import FromHex
+from .from_binary import FromBinary
+from .from_octal import FromOctal
+from .from_decimal import FromDecimal
+from .from_hex import FromHex
 
 __all__ = [
     'FromBinary', 

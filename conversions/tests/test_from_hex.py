5a36383219647e0ad1d7f0eec136d617f18c6098
UzziKyle <uzzielkyleynciong0824@gmail.com>
2024-01-21 11:51:24 +0800

    can now handle fractions; need to work on accuracy

--- conversions/tests/test_from_hex.py
+++ conversions/tests/test_from_hex.py
@@ -33,7 +33,36 @@ class TestFromHex(unittest.TestCase):
         expected_output = 'A'
         
         self.assertEqual(result, expected_output)
-       
+        
+
+class TestFromHexFraction(unittest.TestCase):
+    def test_from_hex_to_binary_fraction(self):
+        data = '1A.C8'
+        result = FromHex().to_binary(data)
+        expected_output = '11010.11001'
+        
+        self.assertEqual(result, expected_output)
+        
+    def test_from_hex_to_octal_fraction(self):
+        data = 'A10.DE'
+        result = FromHex().to_octal(data)
+        expected_output = '5020.674'
+        
+        self.assertEqual(result, expected_output)
+        
+    def test_from_hex_to_decimal_fraction(self):
+        data = 'D1CE.AB'
+        result = FromHex().to_decimal(data)
+        expected_output = '53710.66796875'
+        
+        self.assertEqual(result, expected_output)
+        
+    def test_from_hex_to_hex_fraction(self):
+        data = 'D1CE.AB'
+        result = FromHex().to_hex(data)
+        expected_output = 'D1CE.AB'
+        
+        self.assertEqual(result, expected_output)
         
 if __name__ == '__main__':
     unittest.main()
